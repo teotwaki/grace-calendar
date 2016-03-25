978704f63c80a0b4581d21be1d94c1d1171eddff
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2016-03-25 01:54:48 +0100

    Remove swipe/touch features

--- static/js/app.js
+++ static/js/app.js
@@ -4,19 +4,7 @@
 var app = angular.module('GraceKBHVolunteers', [
   'ngRoute',
   'mobile-angular-ui',
-
-  // touch/drag feature: this is from 'mobile-angular-ui.gestures.js' it is at
-  // a very beginning stage, so please be careful if you like to use in
-  // production. This is intended to provide a flexible, integrated and and
-  // easy to use alternative to other 3rd party libs like hammer.js, with the
-  // final pourpose to integrate gestures into default ui interactions like
-  // opening sidebars, turning switches on/off ..
-  'mobile-angular-ui.gestures'
 ]);
-
-app.run(function($transform) {
-  window.$transform = $transform;
-});
 
 //
 // You can configure ngRoute as always, but to take advantage of SharedState location
