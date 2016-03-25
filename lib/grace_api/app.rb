2d83c511558b5402ebf9880b1ed9c62f6b02537a
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2016-03-25 01:44:08 +0100

    Add mock data foor coming-days API

--- lib/grace_api/app.rb
+++ lib/grace_api/app.rb
@@ -16,5 +16,33 @@ module GraceApi
       content_type :html, 'charset' => 'utf-8'
       send_file File.expand_path('../static/pages/index.html', settings.public_folder)
     end
+
+    get '/coming-days.json' do
+      get_days.collect do |day|
+        {
+          date: day,
+          services: [
+            {
+              type: 'breakfast',
+              help_needed: true,
+            },
+            {
+              type: 'dinner',
+              help_needed: true
+            },
+            {
+              type: 'night',
+              help_needed: true
+            },
+          ]
+        }
+      end.to_json
+    end
+
+    private
+      def get_days
+        day = Date.today
+        (0..6).collect { |x| day + x }
+      end
   end
 end
