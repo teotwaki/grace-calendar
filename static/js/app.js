f5cc026f56121ab36acf57019fed997384308f83
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2016-03-26 20:19:46 +0100

    Add authentication workflow for both Facebook and Google

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@
 //
 angular
   .module('GraceKBHVolunteers',
-          ['ngRoute', 'mobile-angular-ui', 'GraceKBHServices',
+          ['ngRoute', 'mobile-angular-ui', 'GraceKBHServices', 'satellizer',
            'Grace.MyScheduleCtrl', 'Grace.HomeCtrl', 'Grace.MainCtrl', 'Grace.ComingDaysCtrl', 'Grace.LoginCtrl'])
 
 //
@@ -11,7 +11,7 @@ angular
 // feature (i.e. close sidebar on backbutton) you should setup 'reloadOnSearch: false'
 // in order to avoid unwanted routing.
 //
-  .config(function($routeProvider) {
+  .config(function($routeProvider, $authProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'pages/home.html',
@@ -38,4 +38,12 @@ angular
       })
 
       .otherwise('/');
+
+    $authProvider.facebook({
+      clientId: 'foobar'
+    });
+
+    $authProvider.google({
+      clientId: '1055680974535-jfku1jmeokoahn5cflr1q55ekn7ou67t.apps.googleusercontent.com'
+    });
   });
--- static/js/controllers/login.js
+++ static/js/controllers/login.js
@@ -1,4 +1,17 @@
-function LoginCtrl($scope) {
+function LoginCtrl($scope, $auth) {
+  $scope.googleLogin = function() {
+    $auth.authenticate('google')
+      .then(function(response) {
+        console.log(response.data);
+      });
+  }
+
+  $scope.fbLogin = function() {
+    $auth.authenticate('facebook')
+      .then(function(response) {
+        console.log(response.data);
+      });
+  }
 }
 
 angular
