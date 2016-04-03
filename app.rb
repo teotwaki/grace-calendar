1cfe3b2d02573f2f8f6198d3f89a8e475a773ed3
Sebastian Lauwers <sebastian.lauwers@gmail.com>
2016-04-03 13:03:18 +0200

    App can be a simple Sinatra::Base class

--- app.rb
+++ app.rb
@@ -1,16 +1,20 @@
 module Grace
-  class App < Api::Common
+  class App < Sinatra::Base
+    # Automatically serve static content if no match
     set :static, true
     set :public_folder, 'static'
 
+    # Automagically compress output
     use Rack::Deflater
+
+    # Mount the different parts of the API
     use Api::Auth
     use Api::Users
     use Api::Posts
     use Api::ComingDays
 
+    # Serve the main page when requested
     get '/' do
-      # in this case, it shouldn't be JSON
       content_type :html, 'charset' => 'utf-8'
       send_file File.expand_path('pages/index.html', settings.public_folder)
     end
